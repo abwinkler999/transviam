364e58d50b86682f10ef61144df3754919f8bea0
Benjamin Winkler <abwinkler999@gmail.com>
2013-11-10 21:13:55 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transviam::Application.config.secret_token = 'bf0c67227296aac0bf34b8ed809743700b22e4e21add699c352c8327602ad1f468f56ebdcec71dbd4503cace455abf8e5aed34cfdf17552915b7eb8f4d5b6aba'
